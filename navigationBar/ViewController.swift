f70d42fa8d7da0b805a7bbfa4ba681183b86802e
sushmachalamalla <sushma92.ch@gmail.com>
2015-04-20 19:30:53 -0500

    Initial Commit

---
+++ navigationBar/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  navigationBar
+//
+//  Created by Sushma Reddy on 4/20/15.
+//  Copyright (c) 2015 Sushma Reddy. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
